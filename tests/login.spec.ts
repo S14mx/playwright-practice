b66a7764e88f100cffa73cf83ee37705f5079637
Sergii Otryshko <sergey.otryshko@gmail.com>
2023-02-07 00:06:51 -0800

    add tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,13 +3,13 @@ import * as config from '../config/config'
 
 
 
-test('test homepage', async ({ page }) => {
+test.skip('test homepage', async ({ page }) => {
   console.log('testing homepage...')
   await page.goto(config.baseUrl)
   await expect(page.locator(config.homePage_hero_text)).toContainText('Neighbors helping neighbors')
 })
 
-test('test login', async ({ page }) => {
+test.skip('test login', async ({ page }) => {
   console.log('testing login...')
   await page.goto(config.baseUrl)
   await page.locator(config.loginButton_locator).click()
@@ -17,10 +17,32 @@ test('test login', async ({ page }) => {
 })
 
 
-test('test login screenshot', async ({ page }) => {
+test.skip('test login screenshot', async ({ page }) => {
   console.log('testing login screenshot...')
   await page.goto(config.baseUrl)
   await page.locator(config.loginButton_locator).click()
-  await page.waitForTimeout(3000)
-  expect(await page.screenshot({ fullPage: true })).toMatchSnapshot('test-scrn.png');
+  await page.waitForTimeout(6000)
+  expect(await page.screenshot({ fullPage: true })).toMatchSnapshot('test-scrn.png', { maxDiffPixels: 100 });
 })
+
+
+test('test login screenshot_2', async ({ page }) => {
+  console.log('testing login screenshot...')
+  await page.goto(config.baseUrl)
+  await page.locator(config.loginButton_locator).click()
+  await page.waitForTimeout(6000)
+  await expect(page).toHaveScreenshot('test-scrn.png', { maxDiffPixels: 200, fullPage: true });
+})
+
+
+
+test.skip('generated test', async ({ page }) => {
+  await page.goto('https://agreeable-bush-0aea8c110.2.azurestaticapps.net/');
+  await page.getByRole('link', { name: 'Login', exact: true }).click();
+  await page.getByLabel('Email address').click();
+  await page.getByLabel('Email address').fill('vy@gmail.com');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('123456!@#aZ');
+  await page.getByRole('button', { name: 'Continue', exact: true }).click();
+  await expect(page.getByRole('heading', { name: 'Register Form' })).toHaveText("Register Form");
+});
